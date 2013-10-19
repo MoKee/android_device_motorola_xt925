98507318dd904c464dc857614771090daa6bf3f4
martincz <martincz@mokeetek.com>
2013-10-20 00:43:05 +0800

    mokee modify
    Change-Id: If33a0d099e5b39ac0310c0dea49987dfe872eadc

--- mokee.mk
+++ mokee.mk
@@ -1,11 +1,11 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/mk/config/gsm.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 720
@@ -13,7 +13,7 @@ TARGET_SCREEN_HEIGHT := 1280
 
 # Release name
 PRODUCT_RELEASE_NAME := xt925
-PRODUCT_NAME := cm_xt925
+PRODUCT_NAME := mk_xt925
 
 $(call inherit-product, device/motorola/xt925/full_xt925.mk)
 
